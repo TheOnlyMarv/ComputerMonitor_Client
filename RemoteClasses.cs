84c288dc60cafcbc82872e4f015408716e45cf4b
Marvin Vierling <marvinvierling@hotmail.de>
2016-04-28 23:32:42 +0200

    add Newtonsoft add RemoteClasses

---
+++ RemoteClasses.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.Serialization.Json;
+using System.Threading.Tasks;
+
+namespace ComputerMonitorClient.RemoteClasses
+{
+    public class Status
+    {
+        public bool status { get; set; }
+        public string message { get; set; }
+        public string token { get; set; }
+    }
+
+    public class Device
+    {
+        public string id { get; set; }
+        public string name { get; set; }
+    }
+    public class Usage
+    {
+        public string upload { get; set; }
+        public string download { get; set; }
+        public string date { get; set; }
+    }
+}
