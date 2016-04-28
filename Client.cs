e5882db796b7e609675f362420b7ba8de4c3db4b
Marvin Vierling <marvinvierling@hotmail.de>
2016-04-29 00:24:53 +0200

    add client class

---
+++ Client.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComputerMonitorClient
+{
+    public static class Client
+    {
+
+        private static WebClient webClient = new WebClient();
+        private static string baseUrl = @"https://theonlymarv.de/cm/";
+
+        public static string Login(string username, string password)
+        {
+            return webClient.DownloadString(baseUrl + String.Format("login.php?username={0}&password={1}", username, password));
+        }
+    }
+}
