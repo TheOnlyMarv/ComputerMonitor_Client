912f1d3bb43ee7a6c6c7ba1979e77f34f5129114
Vierling, Marvin <marvin.vierling@student.fhws.de>
2016-07-26 12:09:15 +0200

    Add QR code creater

--- Pages/OptionsPage.xaml.cs
+++ Pages/OptionsPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using QRCoder;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
@@ -34,6 +35,24 @@ namespace ComputerMonitorClient
             comboUnit.ItemsSource = Enum.GetValues(typeof(Unit)).Cast<Unit>();
             comboNetworkAdapter.ItemsSource = adapters;
             checkStartup.IsChecked = IsOnStartUp();
+
+            createQrCode();
+        }
+
+        private void createQrCode()
+        {
+            QRCodeGenerator qrGenerator = new QRCodeGenerator();
+            QRCodeData qrData = qrGenerator.CreateQrCode("HIER STEHT EINE IP", QRCodeGenerator.ECCLevel.Q);
+            QRCode qrCode = new QRCode(qrData);
+
+            var bitmap = qrCode.GetGraphic(20);
+            qrImage.Source = System.Windows.Interop.Imaging.CreateBitmapSourceFromHBitmap(
+                bitmap.GetHbitmap(),
+                IntPtr.Zero,
+                Int32Rect.Empty,
+                BitmapSizeOptions.FromEmptyOptions()
+                );
+            //qrImage.Source = ;//BitmapSource.Create(bitmap.Width, bitmap.Height, bitmap.VerticalResolution, bitmap.HorizontalResolution, PixelFormats.Bgr32, null,)
         }
 
         public OptionsPage(IMainSwitch context) : this()
@@ -101,7 +120,7 @@ namespace ComputerMonitorClient
             {
                 Microsoft.Win32.RegistryKey key = Microsoft.Win32.Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);
                 Assembly curAssembly = Assembly.GetExecutingAssembly();
-                return key.GetValue(curAssembly.GetName().Name) != null; 
+                return key.GetValue(curAssembly.GetName().Name) != null;
             }
             catch
             {
